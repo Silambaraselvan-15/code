23476922a70a59bbc5b7786253fa2f4d51a4237f
saransilambu007@gmail.com <saransilambu007@gmail.com>
2025-02-15 21:55:51 +0530

    flower planting problem

---
+++ flower.py
@@ -0,0 +1,25 @@
+"""You have a long flowerbed in which some of the plots are planted, and some are not. 
+
+However,flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, 
+
+where 0 means empty and 1 means not empty, and an integer n, 
+
+return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise."""
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        for i in range(len(flowerbed)):
+            if flowerbed[i]==0:
+                if ((i==0) or (flowerbed[i-1]==0)) and ((i==len(flowerbed)-1) or (flowerbed[i+1]==0)):
+                    flowerbed[i]=1
+                    n-=1
+                    if n==0: 
+                        return True
+
+        return n<=0
+
+sol=Solution()
+print(sol.canPlaceFlowers([1,0,0,0,1],2))
+print(sol.canPlaceFlowers([0, 0, 1, 0, 1], 1)) 
